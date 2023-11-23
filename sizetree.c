3d3a9cfefb56b19839b90beb75c90d16ab6bc052
Jody Bruchon <jody@jodybruchon.com>
2023-11-22 23:29:06 -0500

    Disable some debugging messages

--- sizetree.c
+++ sizetree.c
@@ -27,7 +27,7 @@ static struct sizetree *sizetree_alloc(file_t *file)
   struct sizetree *node;
 
   if (file == NULL) jc_nullptr("sizetree_alloc");
-  LOUD(fprintf(stderr, "sizetree_alloc('%s' [%ld])\n", file->d_name, file->size);)
+//  LOUD(fprintf(stderr, "sizetree_alloc('%s' [%ld])\n", file->d_name, file->size);)
   node = (struct sizetree *)calloc(1, sizeof(struct sizetree));
   if (node == NULL) jc_oom("sizetree_alloc");
   node->size = file->size;
@@ -57,13 +57,13 @@ file_t *sizetree_next_list(int reset)
     st_stack = (struct sizetree **)malloc(sizeof(struct sizetree *) * SIZETREE_ALLOC_SLOTS);
     stackslots = SIZETREE_ALLOC_SLOTS;
     st_stack[0] = sizetree_head;
-fprintf(stderr, "st: stack: pushed X %p\n", sizetree_head);
+//fprintf(stderr, "st: stack: pushed X %p\n", sizetree_head);
     return NULL;
   } else {
     /* Pop one off the stack */
     if (stackcnt < 1) return NULL;
     cur = st_stack[--stackcnt];
-fprintf(stderr, "st: stack: popped %p [%ld]\n", cur, cur->size);
+//fprintf(stderr, "st: stack: popped %p [%ld]\n", cur, cur->size);
     if (cur == NULL) return NULL;
   }
 
@@ -74,9 +74,9 @@ fprintf(stderr, "st: stack: popped %p [%ld]\n", cur, cur->size);
 
   /* Push left/right nodes to stack and return current node's list */
   if (cur->right != NULL) st_stack[stackcnt++] = cur->right;
-if (cur->right != NULL) fprintf(stderr, "st: stack: pushed R %p\n", st_stack[stackcnt - 1]);
+//if (cur->right != NULL) fprintf(stderr, "st: stack: pushed R %p\n", st_stack[stackcnt - 1]);
   if (cur->left != NULL) st_stack[stackcnt++] = cur->left;
-if (cur->left != NULL) fprintf(stderr, "st: stack: pushed L %p\n", st_stack[stackcnt - 1]);
+//if (cur->left != NULL) fprintf(stderr, "st: stack: pushed L %p\n", st_stack[stackcnt - 1]);
   return cur->list;
 }
 
@@ -89,7 +89,7 @@ void sizetree_add(file_t *file)
   LOUD(fprintf(stderr, "sizetree_add([%p] '%s')\n", file, file->d_name);)
 
   if (sizetree_head == NULL) {
-    LOUD(fprintf(stderr, "sizetree_add: new sizetree_head size %ld\n", file->size);)
+//    LOUD(fprintf(stderr, "sizetree_add: new sizetree_head size %ld\n", file->size);)
     sizetree_head = sizetree_alloc(file);
     return;
   }
@@ -97,7 +97,7 @@ void sizetree_add(file_t *file)
   cur = sizetree_head;
   while (1) {
     if (file->size == cur->size) {
-      LOUD(fprintf(stderr, "sizetree_add: attaching to list [%p] size %ld\n", cur, cur->size);)
+//      LOUD(fprintf(stderr, "sizetree_add: attaching to list [%p] size %ld\n", cur, cur->size);)
       file->next = cur->list;
       cur->list = file;
       return;
@@ -106,10 +106,10 @@ void sizetree_add(file_t *file)
     if (file->size < cur->size) {
       if (cur->left == NULL) {
         cur->left = sizetree_alloc(file);
-        LOUD(fprintf(stderr, "sizetree_add: creating new list L [%p] size %ld\n", cur->left, file->size);)
+//        LOUD(fprintf(stderr, "sizetree_add: creating new list L [%p] size %ld\n", cur->left, file->size);)
 	return;
       }
-      LOUD(fprintf(stderr, "sizetree_add: going left (%ld < %ld)\n", file->size, cur->size);)
+//      LOUD(fprintf(stderr, "sizetree_add: going left (%ld < %ld)\n", file->size, cur->size);)
       cur = cur->left;
       continue;
     }
@@ -117,10 +117,10 @@ void sizetree_add(file_t *file)
     if (file->size > cur->size) {
       if (cur->right == NULL) {
         cur->right = sizetree_alloc(file);
-        LOUD(fprintf(stderr, "sizetree_add: creating new list R [%p] size %ld\n", cur->right, file->size);)
+//        LOUD(fprintf(stderr, "sizetree_add: creating new list R [%p] size %ld\n", cur->right, file->size);)
 	return;
       }
-      LOUD(fprintf(stderr, "sizetree_add: going right (%ld > %ld)\n", file->size, cur->size);)
+//      LOUD(fprintf(stderr, "sizetree_add: going right (%ld > %ld)\n", file->size, cur->size);)
       cur = cur->right;
       continue;
     }
