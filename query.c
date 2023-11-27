e53d43c7676fc2079a78a2b71fafe582614fe379
Jody Bruchon <jody@jodybruchon.com>
2023-11-27 18:03:17 -0500

    Add query.c/h skeleton

---
+++ query.c
@@ -0,0 +1,15 @@
+/* Query match data
+ * This file is part of jdupes; see jdupes.c for license information */
+#include <stdio.h>
+#include <stdint.h>
+
+#include <libjodycode.h>
+#include "jdupes.h"
+#include "sizetree.h"
+#include "query.h"
+
+
+file_t *query_next_match(int reset)
+{
+	return NULL;
+}
---
+++ query.h
@@ -0,0 +1,20 @@
+/* Query match data
+ * This file is part of jdupes; see jdupes.c for license information */
+
+#ifndef JDUPES_QUERY_H
+#define JDUPES_QUERY_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct querystate {
+  file_t *curfile;
+  // FIXME: finish this
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* JDUPES_QUERY_H */
