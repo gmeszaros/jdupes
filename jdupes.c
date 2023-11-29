ad5f8655d6324be55a66680fdba98f187dc07b95
Jody Bruchon <jody@jodybruchon.com>
2023-11-28 23:07:12 -0500

    Simplify loop

--- jdupes.c
+++ jdupes.c
@@ -798,9 +798,8 @@ skip_file_scan:
   fprintf(stderr, "Dupe chains: %d, headed: %d\n", chains, headed);
 #endif  // 0
   st_state.stackcnt = -1;
-  st_next = sizetree_next_list(&st_state);
-  while (st_next != NULL) {
-    for (int j = 0; st_next != NULL; j++, st_next = st_next->next) {
+  for (st_next = sizetree_next_list(&st_state); st_next != NULL; st_next = sizetree_next_list(&st_state)) {
+    for (; st_next != NULL; st_next = st_next->next) {
       if (ISFLAG(st_next->flags, FF_DUPE_CHAIN_HEAD)) {
         for (file_t *cur = st_next; cur != NULL; cur = cur->duplicates) {
           printf("%s\n", cur->d_name);
@@ -808,7 +807,6 @@ skip_file_scan:
 	printf("\n");
       }
     }
-    st_next = sizetree_next_list(&st_state);
   }
 
 
