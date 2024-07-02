84ef251c037d44ab799300b6747e6c3559b006e8
Jody Bruchon <jody@jodybruchon.com>
2024-07-02 17:19:19 -0400

    Time result verifications

--- verify_results.sh
+++ verify_results.sh
@@ -3,6 +3,6 @@
 [[ -z "$@" ]] && echo "Specify options and dirs" && exit 1
 
 echo -n "Installed: "
-jdupes $@ 2>/dev/null | sort -g | md5sum
+time jdupes $@ 2>/dev/null | sort -g | md5sum
 echo -n "Built:     "
-./jdupes $@ 2>/dev/null | sort -g | md5sum
+time ./jdupes $@ 2>/dev/null | sort -g | md5sum
